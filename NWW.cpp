7534e3b2a740d5aaf845918852c768d4a805983f
OskarJ1 <lolinkross59@gmail.com>
2022-04-12 14:41:07 +0200

    NWW

---
+++ NWW.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+int x(int a, int b)
+{
+	while (a != b) {
+		if (b > a)
+			b = b - a;
+		else
+			a = a - b;
+	}
+	return a;
+}
+int main()
+{
+	int a, b;
+	cout << "Podaj liczy";
+	cin >> a >> b;
+	cout << "NWD z " << a << "," << b << " = " << x(a, b) << endl;
+
+
+
+	return 0;
+}
